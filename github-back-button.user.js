20430681c34bf4ac42fba9cdb4fa9567388cc590
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2015-12-11 17:15:34 -0500

    Create github-back-button.user.js

---
+++ github-back-button.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        Github: Back button's back, all right!
+// @namespace   http://github.com/carols10cents/github-back-button
+// @description Restores the functionality of the back button on github pull requests when navigating to the commits/files tabs.
+// @include     http://github.com/*
+// @include     https://github.com/*
+// @include     http://www.github.com/*
+// @include     https://www.github.com/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+$(".js-pull-request-tab").removeClass("js-pull-request-tab");
